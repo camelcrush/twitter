c3570757be93c1fc39aceec51af5c8f8303a38c1
camelcrush <dkxm0927@naver.com>
2021-11-07 20:53:30 +0900

    Router: HashRouter, Route, Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import firebase from "./firebase";
+import AppRouter from "./Router";
 
 function App() {
-  return <div></div>;
+  return <AppRouter />;
 }
 
 export default App;
---
+++ src/routes/EditProfile.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const EditProfile = () => <span></span>;
+
+export default EditProfile;
