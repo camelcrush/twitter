6b7e3962177dbd121551850f7569ae29f022d813
camelcrush <dkxm0927@naver.com>
2021-11-08 22:31:54 +0900

    Social Login: provider, signInWithPopup()

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 import { auth } from "firebase";
 
@@ -23,20 +26,29 @@ const Auth = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
+      let data;
       if (newAccount) {
-        const data = await createUserWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
+        data = await createUserWithEmailAndPassword(auth, email, password);
       } else {
-        const data = await signInWithEmailAndPassword(auth, email, password);
+        data = await signInWithEmailAndPassword(auth, email, password);
       }
     } catch (error) {
       setError(error.message);
     }
   };
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    const data = await signInWithPopup(auth, provider);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -66,8 +78,12 @@ const Auth = () => {
         {newAccount ? "Sign In" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
